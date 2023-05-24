19796039bc2140ed2296a8024fc36920bbd5861a
VyacheslavSirikov <s.sirikov28@gmail.com>
2023-05-24 20:01:57 +0300

    aaa

--- app/src/main/java/com/example/shipwar/MainActivity.java
+++ app/src/main/java/com/example/shipwar/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.shipwar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView vAx1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vAx1 = findViewById(R.id.vAx1);
+        vAx1.setOnClickListener(v -> {
+            vAx1.setText("0");
+        });
     }
 }
